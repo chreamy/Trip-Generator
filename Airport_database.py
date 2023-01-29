f7fbc44a69c28ecffe2e048f13250a60881f393c
chris <116124688+kiyoyoneko@users.noreply.github.com>
2023-01-29 07:15:42 -0600

    mark's changes

--- Airport_database.py
+++ Airport_database.py
@@ -1,17 +1,38 @@
-from Airport_information import airport_information
+from airport import airport
 
 
-def import_database():
+def load_airport_database():
     database = {}
-    IATA_codes = set()
 
-    with open('Airport_database.txt') as file:
+    with open('airport_database.txt') as file:
         data = file.read().splitlines()
         for line in data:
             line = line.split(':')
-            IATA_codes.add(line[1])
-            database.update({line[1]: airport_information(name=line[2],
-                                                          city=line[3],
-                                                          lat=line[14],
-                                                          long=line[15])})
+            database.update({line[1]: airport(IATA=line[1],
+                                              name=line[2],
+                                              city=line[3],
+                                              lat=line[14],
+                                              long=line[15])})
+
     return database
+
+
+db = load_airport_database()
+
+
+def city(IATA):
+    return db.get(IATA).city
+
+
+def name(IATA):
+    return db.get(IATA).name
+
+
+
+def lat(IATA):
+    return float(db.get(IATA).lat)
+
+
+def long(IATA):
+    return float(db.get(IATA).long)
+
