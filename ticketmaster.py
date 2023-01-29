62e8992be5e37f0e29a85c25ba5539ad0277b379
superspaceturtle <122420958+superspaceturtle@users.noreply.github.com>
2023-01-28 21:33:41 -0600

    Create ticketmaster.py

---
+++ ticketmaster.py
@@ -0,0 +1,14 @@
+import ticketpy
+
+tm_client = ticketpy.ApiClient('45LwqqL0VU4G5PGAIsAnYebkwpNRQpGf')
+
+pages = tm_client.events.find(
+    classification_name='sports',
+    city='Atlanta',
+    start_date_time='2023-02-15T20:00:00Z',
+    end_date_time='2023-02-20T20:00:00Z'
+)
+
+for page in pages:
+    for event in page:
+        print(event)
